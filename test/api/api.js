fe91cccbed65399653eb7dedf9a7399ee8f46dc0
bestan <bestan93@gmail.com>
2014-11-11 21:25:45 +0000

    Fixed API save tests

--- test/api/api.js
+++ test/api/api.js
@@ -21,6 +21,51 @@ describe('API', function() {
         });
     });
 
+    describe('When I make save type request', function() {
+        it('should save type', function(done) {
+            globals.sdk.call({
+                "function": "save_type",
+                "parameters": {
+                    "type" : {
+                        name: "person",
+                        display_name: "Person",
+                        type: "object",
+                        fields:[
+                            {
+                                name: "first_name",
+                                display_name: "First Name",
+                                type: "text",
+                                min_length: 3
+                            },{
+                                name: "last_name",
+                                display_name: "Last Name",
+                                type: "text",
+                                min_length: 3
+                            },{
+                                name: "office_number",
+                                display_name: "Office Number",
+                                type: "number",
+                                description: "Please enter your office number",
+                                minimum: 1,
+                                maximum: 30,
+                                integer: true
+                            }
+                        ]
+                    }
+                }
+            }, function(error, response) {
+                logger.log(error, response);
+                assert.equal(error, null);
+
+                globals.sdk.get(["/Mino/types/person"], function(err, res) {
+                    var person_type = res.objects[0];
+                    assert.notEqual(person_type, null);
+                    done();
+                })
+            });
+        });
+    });
+
     describe('When I make a simple save request', function() {
         it('should save an object', function(done) {
             globals.sdk.with_user("testuser").call({
@@ -30,8 +75,8 @@ describe('API', function() {
                         "name": "TestSave",
                         "path":"/testuser/",
                         "person":{
-                            "first_name":"Marcus",
-                            "last_name":"Longmuir",
+                            "first_name": "Marcus",
+                            "last_name": "Longmuir",
                             "office_number" : 25
                         }
                     }]
@@ -61,51 +106,6 @@ describe('API', function() {
                 logger.log(JSON.stringify(error, null, 4), response);
                 assert.notEqual(error,null)
                 done();
-            });
-        });
-    });
-
-    describe('When I make save type request', function() {
-        it('should save type', function(done) {
-            globals.sdk.call({
-                "function": "save_type",
-                "parameters": {
-                    "type" : {
-                        name: "person",
-                        display_name: "Person",
-                        type: "object",
-                        fields:[
-                            {
-                                name: "first_name",
-                                display_name: "First Name",
-                                type: "text",
-                                min_length: 10
-                            },{
-                                name: "last_name",
-                                display_name: "Last Name",
-                                type: "text",
-                                max_length: 3
-                            },{
-                                name: "office_number",
-                                display_name: "Office Number",
-                                type: "number",
-                                description: "Please enter your office number",
-                                minimum: 1,
-                                maximum: 30,
-                                integer: true
-                            }
-                        ]
-                    }
-                }
-            }, function(error, response) {
-                logger.log(error, response);
-                assert.equal(error, null);
-
-                globals.sdk.get(["/Mino/types/person"], function(err, res) {
-                    var person_type = res.objects[0];
-                    assert.notEqual(person_type, null);
-                    done();
-                })
             });
         });
     });
