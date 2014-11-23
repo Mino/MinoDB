4469c66d31215f61ca28de6faa7a4cb28acb249f
bestan <bestan93@gmail.com>
2014-11-23 16:10:17 +0000

    Testing presence of default plugins

--- test/plugins/plugins.js
+++ test/plugins/plugins.js
@@ -2,51 +2,81 @@ var globals = require('../globals');
 var logger = require('tracer').console();
 var assert = require('assert');
 var express = require('express');
-var request = require('supertest');
+var request_plugin_config = require('./request_plugin_config');
 
-describe("Plugin", function() {
-	it("should be added", function(done) {
-		var PLUGIN_RESPONSE = "Testing plugin response";
+describe("Plugins", function() {
 
-		var server = express();
-		server.get("/", function(req, res) {
-			res.send(PLUGIN_RESPONSE);
-		})
-		server.use("/mino/", globals.mino.server());
+	describe("New plugin", function() {
 
-		var plugin = {
-			get_config_server: function() {
-				return server;
-			},
-			info: function() {
-				return {
-					name: "test_plugin",
-					display_name: "Test Plugin"
+		it("should be added", function(done) {
+			var PLUGIN_RESPONSE = "Testing plugin response";
+
+			var server = express();
+			server.get("/", function(req, res) {
+				res.send(PLUGIN_RESPONSE);
+			})
+
+			var plugin = {
+				get_config_server: function() {
+					return server;
+				},
+				info: function() {
+					return {
+						name: "test_plugin",
+						display_name: "Test Plugin"
+					}
+				},
+				init: function() {
+
 				}
-			},
-			init: function() {
-
 			}
-		}
 
-		globals.mino.add_plugin(plugin);
+			var test_server = express()
+			test_server.use("/mino/", globals.mino.server());
 
-		var agent = request.agent(server);
-
-		agent
-			.post('/mino/ajax/login')
-			.send({username_or_email: 'testuser', password: 'my_password'})
-			.expect(200)
-			.end(function(err, res) {
-
-				agent
-					.get('/mino/admin/plugin_config/test_plugin')
-					.expect(200)
-					.end(function(err, res) {
-						logger.log(res.body, res.text);
-						assert.equal(res.text, PLUGIN_RESPONSE);
-						done();
-					});
-			});
+			globals.mino.add_plugin(plugin);
+			request_plugin_config("test_plugin", test_server, function(err, res) {
+				assert.equal(err, null);
+				assert.equal(res, PLUGIN_RESPONSE);
+				done();
+			})
+			
+		});
 	});
+
+	describe("API plugin", function() {
+		it("should be added", function(done) {
+			var test_server = express()
+			test_server.use("/mino/", globals.mino.server());
+			request_plugin_config("api", test_server, function(err, res) {
+				assert.equal(err, null);
+				assert.equal(res, "API CONFIG");
+				done();
+			})
+		})
+	})
+
+	describe("Browser plugin", function() {
+		it("should be added", function(done) {
+			var test_server = express()
+			test_server.use("/mino/", globals.mino.server());
+			request_plugin_config("browser", test_server, function(err, res) {
+				assert.equal(err, null);
+				assert.notEqual(res.indexOf('/mino/admin/plugin_config/browser/frontend.js'), -1);
+				done();
+			})
+		})
+	})
+
+	describe("UI plugin", function() {
+		it("should be added", function(done) {
+			var test_server = express()
+			test_server.use("/mino/", globals.mino.server());
+			request_plugin_config("ui", test_server, function(err, res) {
+				assert.equal(err, null);
+				assert.equal(res, "UI CONFIG");
+				done();
+			})
+		})
+	})
 });
