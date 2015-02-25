aa47432c1d64ef69a89c239891ee0275c936e083
bestan <bestan93@gmail.com>
2015-02-25 14:30:56 +0200

    Updated fieldval-ui refrence from node_modules to bower_components

--- default_plugins/browser_server/admin/frontend.js
+++ default_plugins/browser_server/admin/frontend.js
@@ -18448,6 +18448,44 @@ FVEmailRuleField.prototype.init = function() {
 if (typeof module != 'undefined') {
     module.exports = FVEmailRuleField;
 }
+if((typeof require) === 'function'){
+    extend = require('extend')
+    FVBasicRuleField = require('./FVBasicRuleField');
+}
+extend(FVDateRuleField, FVBasicRuleField);
+
+function FVDateRuleField(json, validator) {
+    var field = this;
+
+    FVDateRuleField.superConstructor.call(this, json, validator);
+}
+
+FVDateRuleField.prototype.create_ui = function(parent){
+    var field = this;
+
+    field.ui_field = new FVDateField(field.display_name || field.name, {
+        name: field.json.name,
+        display_name: field.json.display_name,
+        format: field.date_format
+    });
+
+    field.element = field.ui_field.element;
+    return field.ui_field;
+}
+
+FVDateRuleField.prototype.init = function() {
+    var field = this;
+
+    field.date_format = field.validator.get("format", BasicVal.string(true), BasicVal.date_format({emit:FieldVal.DateVal.EMIT_STRING}));
+    if (field.date_format !== undefined) {
+        field.checks.push(BasicVal.date(field.date_format));
+    }
+    return field.validator.end();
+}
+
+if (typeof module != 'undefined') {
+    module.exports = FVDateRuleField;
+}
 
 if((typeof require) === 'function'){
     FieldVal = require('fieldval')
@@ -18572,6 +18610,11 @@ FVRuleField.add_field_type({
     name: 'email',
     display_name: 'Email',
     class: (typeof FVEmailRuleField) !== 'undefined' ? FVEmailRuleField : require('./fields/FVEmailRuleField')
+});
+FVRuleField.add_field_type({
+    name: 'date',
+    display_name: 'Date',
+    class: (typeof FVDateRuleField) !== 'undefined' ? FVDateRuleField : require('./fields/FVDateRuleField')
 });
 //Used to subclass Javascript classes
 function extend(sub, sup) {
