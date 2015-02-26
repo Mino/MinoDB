c4f280fdb3bde7eeba9d0e745742481a94fbbf5b
bestan <bestan93@gmail.com>
2015-02-26 15:55:55 +0200

    Sending static methods for custom rules excluding superConstructor

--- MinoDB/class_to_string.js
+++ MinoDB/class_to_string.js
@@ -4,9 +4,17 @@ module.exports = function(class_data){
 
     var class_string = String(class_data)+"\n\n";
 
+    console.log(class_name);
+    for (var i in class_data) {
+    	if (i != "superConstructor" && class_data.hasOwnProperty(i) && typeof class_data[i] == "function") {
+    		console.log(i, typeof class_data[i], class_data[i]);
+	    	class_string+=class_name+"."+i+" = "+String(class_data[i]) + "\n\n";
+    	}
+    }
+
     var proto = class_data.prototype;
     for(var i in proto){
-        class_string+=class_name+".prototype."+i+" = "+String(proto[i])+"\n\n"
+        class_string+=class_name+".prototype."+i+" = "+String(proto[i])+"\n\n";
     }
 
     return class_string;
