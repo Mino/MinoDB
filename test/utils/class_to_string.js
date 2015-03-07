fdf6768294958ed6abcda97be734168b65dea095
Marcus Longmuir <marcuslongmuir@me.com>
2015-03-07 00:14:05 +0000

    Added test for instance and static methods with the same name

--- test/utils/class_to_string.js
+++ test/utils/class_to_string.js
@@ -27,6 +27,17 @@ describe("class_to_string", function() {
 		assert.equal(output, "function test_class() {}\n\ntest_class.method = function (b){b+=2}\n\n");
 	})
 
+	it("should include instance and static methods with the same name", function() {
+
+		function test_class() {}
+		test_class.prototype.method = function(b){b+=2};
+		test_class.method = function(c){c+=2};
+
+		var output = class_to_string(test_class);
+		assert.notEqual(output.indexOf("test_class.prototype.method = function (b){b+=2}"), -1);
+		assert.notEqual(output.indexOf("test_class.method = function (c){c+=2}"), -1);
+		assert.equal(output, "function test_class() {}\n\ntest_class.method = function (c){c+=2}\n\ntest_class.prototype.method = function (b){b+=2}\n\n");
+	})
 
 	it("should include class properties", function() {
 
