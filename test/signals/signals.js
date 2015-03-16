8f5cb6619b6c5d351d8283f45c3eec4973acb9ea
bestan <bestan93@gmail.com>
2015-03-16 15:39:56 +0200

    Refactored Signals

--- test/signals/signals.js
+++ test/signals/signals.js
@@ -1,6 +1,7 @@
 var logger = require('tracer').console();
 var assert = require('assert');
 var globals = require('../globals');
+var Signal = require('../../MinoDB/core/models/Signal');
 
 describe("Signals", function() {
 
@@ -23,13 +24,85 @@ describe("Signals", function() {
 		})
 	})
 
-	afterEach(function() {
-		//Clearing signals
-		globals.mino.signal_manager.static_signals = []
-		globals.mino.signal_manager.dynamic_signal_callbacks = {}
+	it("should trigger when path and handler match", function(done) {
+
+		var save_object = {
+			name: "signal_object",
+			path: "/testuser/folder/",
+		}
+		
+		var signal = new Signal({
+			paths: ["/testuser/folder/"],
+			include_subfolders: false,
+			handlers: ["save"],
+			callback: function(object) {
+				assert.equal(object.name, save_object.name);
+				assert.equal(object.path, save_object.path);
+				done();
+			}
+		})
+
+		globals.mino.add_signal(signal);
+
+		globals.sdk.with_user("testuser").save([save_object], function(err, res) {
+			assert.equal(err, null);
+		})
+
 	})
 
-	require('./static_signals');
-	require('./dynamic_signals');
+	it("should trigger when include_subfolders=true and path is a child of signal path", function(done) {
+
+		var save_object = {
+			name: "signal_object_in_subfolder",
+			path: "/testuser/folder/inner_folder/",
+		}
+
+		var signal = new Signal({
+			paths: ["/testuser/folder/"],
+			include_subfolders: true,
+			handlers: ["save"],
+			callback: function(object) {
+				assert.equal(object.name, save_object.name);
+				assert.equal(object.path, save_object.path);
+				done();
+			}
+		})
+			
+		globals.mino.add_signal(signal);
+
+		globals.sdk.with_user("testuser").save([save_object], function(err, res) {
+			assert.equal(err, null);
+		})
+
+	})
+
+	it("should trigger when no path specified", function(done) {
+		var save_object = {
+			name: "signal_object_no_path",
+			path: "/testuser/folder/inner_folder/",
+		}
+
+		var signal = new Signal({
+			handlers: ["save"],
+			callback: function(object) {
+				assert.equal(object.name, save_object.name);
+				assert.equal(object.path, save_object.path);
+				done();
+			}
+		})
+			
+		globals.mino.add_signal(signal);
+
+		globals.sdk.with_user("testuser").save([save_object], function(err, res) {
+			assert.equal(err, null);
+		})
+
+	});
+
+
+	afterEach(function() {
+		//Clearing signals
+		globals.mino.signal_manager.signals = []
+	})
 
 })
